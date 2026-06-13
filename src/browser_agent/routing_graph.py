06530a8dfb44a1461f6d258a0b27e8075b64c958
Nikolay <golikov.dev@gmail.com>
2026-06-13 18:04:39 +0300

    task 2 completed

---
+++ src/browser_agent/routing_graph.py
@@ -0,0 +1,25 @@
+"""Conditional LangGraph from lesson 3.
+
+Implement the router, terminal nodes, and graph described in
+docs/lessons/03-conditional-routing.md.
+"""
+
+
+def route_start_url(state):
+    """TODO: return the name of the next node."""
+    raise NotImplementedError
+
+
+def pass_run(state):
+    """TODO: return a successful status update."""
+    raise NotImplementedError
+
+
+def fail_run(state):
+    """TODO: return a failed status update."""
+    raise NotImplementedError
+
+
+def build_routing_graph():
+    """TODO: compile the conditional learning graph."""
+    raise NotImplementedError
